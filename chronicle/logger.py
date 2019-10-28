6d42beaa9bce4b876524c29661cf8fbe02b0e0af
georgepsarakis <giwrgos.psarakis@gmail.com>
2019-10-28 14:40:28 +0200

    Uniform log entries as JSON-serialized events

---
+++ chronicle/logger.py
@@ -0,0 +1,18 @@
+import os
+import json
+
+
+_MAIN_PROCESS_IDENTIFIER = 'COORDINATOR'
+
+
+def generate(message, task_id=None, pid=None, stream=None, **kwargs):
+    pid = pid or os.getpid()
+    task_id = task_id or _MAIN_PROCESS_IDENTIFIER
+    entry = {
+        'message': str(message),
+        'task_id': task_id,
+        'pid': str(pid),
+        'stream': stream
+    }
+    entry.update({key: str(value) for key, value in kwargs.items()})
+    return json.dumps(entry)
