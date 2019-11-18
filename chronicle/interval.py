5afcc7c7cb44b27c63abf48cea782a5d6ce478f5
georgepsarakis <giwrgos.psarakis@gmail.com>
2019-11-18 19:25:50 +0200

    Pending check should compare with equality
    - Next run should be initialized to croniter next schedule.

--- chronicle/interval.py
+++ chronicle/interval.py
@@ -8,10 +8,11 @@ logger = logging.getLogger(__name__)
 
 
 class CronInterval:
-    def __init__(self, expression: str, base_time: float):
+    def __init__(self, expression: str, base_time: int):
         # TODO: add validation
         self._expression = expression
         self._iterator = None
+        assert base_time is not None
         self._base_time = base_time
         self._next_run_at = None
 
@@ -33,15 +34,16 @@ class CronInterval:
             self._iterator = croniter(self.expression, self.base_time)
         return self._iterator
 
-    def is_pending(self, current_time: float):
-        return self.iterator.get_current() <= current_time
+    def is_pending(self, current_time: int):
+        return self.next_run_at == int(current_time)
 
     @property
     def next_run_at(self):
-        return self._next_run_at or self.iterator.get_current()
+        return self._next_run_at or self.schedule_next()
 
     def schedule_next(self):
         self._next_run_at = self.iterator.get_next()
+        return self._next_run_at
 
     def parse_expression(self):
         options = Options()
