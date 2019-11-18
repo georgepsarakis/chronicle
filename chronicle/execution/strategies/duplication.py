bb96161e87b393efaa7ca544fd4b7e5c82b39ffb
georgepsarakis <giwrgos.psarakis@gmail.com>
2019-11-18 20:25:47 +0200

    Define duplicate command handling strategies

---
+++ chronicle/execution/strategies/duplication.py
@@ -0,0 +1,40 @@
+import logging
+
+from chronicle.execution.strategies.base import StrategyBase
+import chronicle.logger as log_helper
+
+logger = logging.getLogger(__name__)
+
+
+class SkipCommand(StrategyBase):
+    def process(self, command):
+        if self.already_running(command):
+            logger.warning(
+                log_helper.generate(
+                    'skipped',
+                    str(command),
+                    tags=['duplicate_command', 'job', 'filtering']
+                )
+            )
+            return False
+        return True
+
+
+class RestartCommand(StrategyBase):
+    def _find_running_instance(self, command):
+        for job_instance in self._running_commands:
+            if command == job_instance:
+                return job_instance
+
+    def process(self, command):
+        if self.already_running(command):
+            running_instance = self._find_running_instance(command)
+            running_instance.process.terminate()
+            logger.warning(
+                log_helper.generate(
+                    'terminated',
+                    str(command),
+                    tags=['duplicate_command', 'job', 'filtering']
+                )
+            )
+        return True
