4d100eae697151c151002339d33387e1dcaa8ac4
georgepsarakis <giwrgos.psarakis@gmail.com>
2019-11-30 13:35:10 +0200

    Use Application class in CLI

--- chronicle/cli.py
+++ chronicle/cli.py
@@ -2,12 +2,9 @@ import sys
 import logging
 
 import click
+from dynaconf import settings
 
-from chronicle.job import Job
-from chronicle.crontab import Crontab
-from chronicle.backend import RedisBackend, StrictRedis
-from chronicle.execution.strategies.duplication import \
-    SkipCommand, RestartCommand
+from chronicle.application import Application
 
 logger = logging.getLogger(__name__)
 
@@ -20,84 +17,6 @@ logging.basicConfig(
     datefmt="%Y-%m-%dT%H:%M:%S"
 )
 
-configuration = {
-    'jobs': [{
-        'interval': '*/2 * * * *',
-        'command': 'for i in 1 2 3 4 5 6 7 8 9 10; do date; sleep 15; done',
-        'bash': True,
-        'environment': 'INHERIT_ALL',
-        'timeout': None
-    # }, {
-    #     'interval': '*/1 * * * *',
-    #     'command': 'date && env',
-    #     'bash': True,
-    #     'environment': 'INHERIT_WHITELISTED_ONLY'
-    }, #{
-    #     'interval': '* * * * *',
-    #     'command': 'env | grep CHRONICLE',
-    #     'bash': True,
-    #     'environment': 'INHERIT_WHITELISTED_ONLY'
-    # }, # {
-        # 'interval': '*/9 * * * *',
-        # 'command': 'ls',
-        # 'bash': True,
-        # 'environment': 'INHERIT_WHITELISTED_ONLY'
-#    }
-],
-    'backend': {
-        'url': 'redis://localhost:6379'
-    },
-    'execution': {
-        'strategies': {
-            'duplication': 'skip'
-        }
-    }
-}
-
-Crontab.setup(
-    jobs=[
-        Job.from_dict(parameters)
-        for parameters in configuration['jobs']
-    ]
-)
-
-
-def _integer_option(_ctx, _param, value):
-    if value is not None:
-        return int(value)
-    return value
-
-
-def _get_nested_configuration(config, *path):
-    c = config
-    for segment in path:
-        c = c.get(segment, {})
-    return c
-
-def _initialize_crontab(conf):
-    strategies = []
-    duplication_strategy = _get_nested_configuration(
-        conf,
-        'execution',
-        'strategies',
-        'duplication'
-    )
-    if duplication_strategy:
-        if duplication_strategy == 'skip':
-            strategies.append(SkipCommand)
-        elif duplication_strategy == 'restart':
-            strategies.append(RestartCommand)
-
-    parameters = {}
-    if 'backend' in conf:
-        parameters['backend'] = \
-            RedisBackend(StrictRedis.from_url(conf['backend'].get('url')))
-
-    if strategies:
-        parameters['execution_strategies'] = strategies
-
-    return Crontab(**parameters)
-
 
 @click.group()
 def cli():
@@ -106,26 +25,27 @@ def cli():
 
 @cli.command()
 @click.option('--backfill-since',
-              callback=_integer_option,
-              default=None,
+              default=0,
+              show_default=True,
               help='Use this timestamp as a starting point for '
                    'scheduling and executing tasks.')
 def start(backfill_since):
-    crontab = _initialize_crontab(configuration)
+    crontab = Application(settings).create().crontab
     crontab.start(backfill_since)
 
 
 @cli.command()
-@click.option('--interval', callback=_integer_option)
-@click.option('--pattern', default=None)
-def pause(interval, pattern):
-    crontab = _initialize_crontab(configuration)
+@click.option('--interval',
+              default=60,
+              show_default=True)
+def pause(interval):
+    crontab = Application(settings).create().crontab
     crontab.pause(interval)
 
 
 @cli.command()
 def resume():
-    crontab = _initialize_crontab(configuration)
+    crontab = Application(settings).create().crontab
     crontab.resume()
 
 
